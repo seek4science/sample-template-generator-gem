a3aeca600ab308d32f326935aeb994d022938780
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-12-14 14:00:40 +0000

    OPSK-1475 - the entry method also needs to take the base template path

--- lib/seek/sample_templates.rb
+++ lib/seek/sample_templates.rb
@@ -5,16 +5,18 @@ module Seek
   # Main entry point for generating templates
   module SampleTemplates
     # generates a template at the specific path
-    def self.generate(sheet_name, sheet_index, columns, path)
+    def self.generate(sheet_name, sheet_index, columns, base_template_path, output_path)
       Seek::SampleTemplates::Generator.new(
-        path, create_json(columns, sheet_index, sheet_name)
+          output_path, create_json(sheet_name,sheet_index,columns,base_template_path)
       ).generate
     end
 
-    def self.create_json(columns, sheet_index, sheet_name)
-      { sheet_name: sheet_name,
+    def self.create_json(sheet_name,sheet_index,columns,base_template_path)
+      definition = { sheet_name: sheet_name,
         sheet_index: sheet_index,
-        columns: columns.collect(&:as_json) }.to_json
+        columns: columns.collect(&:as_json) }
+      definition[:base_template_path]=base_template_path if base_template_path
+      definition.to_json
     end
 
     private_class_method :create_json
--- test/sample_template_generator_test.rb
+++ test/sample_template_generator_test.rb
@@ -13,7 +13,7 @@ class SampleTemplateGeneratorTest < Minitest::Test
 
   def test_main_entry
     refute(File.exist?(@path))
-    Seek::SampleTemplates.generate('fred', 0, [], @path)
+    Seek::SampleTemplates.generate('fred', 0, [], nil,@path)
     assert(File.exist?(@path))
   end
 
@@ -28,7 +28,7 @@ class SampleTemplateGeneratorTest < Minitest::Test
   def test_generate_with_columns_names
     refute(File.exist?(@path))
     columns = [Seek::SampleTemplates::Column.new('fred'), Seek::SampleTemplates::Column.new('barry'), Seek::SampleTemplates::Column.new('alice')]
-    Seek::SampleTemplates.generate('samples', 0, columns, @path)
+    Seek::SampleTemplates.generate('samples', 0, columns, nil, @path)
     assert(File.exist?(@path))
   end
 
@@ -47,8 +47,24 @@ class SampleTemplateGeneratorTest < Minitest::Test
   def test_with_quotes_in_columns
     refute(File.exist?(@path))
     columns = [Seek::SampleTemplates::Column.new("with a ' quote")]
-    Seek::SampleTemplates.generate('samples', 0, columns, @path)
+    Seek::SampleTemplates.generate('samples', 0, columns,nil, @path)
     assert(File.exist?(@path))
+  end
+
+  def test_with_base_template
+    refute(File.exist?(@path))
+    columns = [Seek::SampleTemplates::Column.new("with a ' quote")]
+    Seek::SampleTemplates.generate('samples', 0, columns,base_template_path, @path)
+    assert(File.exist?(@path))
+  end
+
+  def test_create_json_with_or_without_base_template
+    columns = [Seek::SampleTemplates::Column.new("col")]
+    json = Seek::SampleTemplates.send(:create_json,'fish',2,columns,nil)
+    assert_equal %!{"sheet_name":"fish","sheet_index":2,"columns":[{"col":[]}]}!,json
+
+    json = Seek::SampleTemplates.send(:create_json,'fish',2,columns,'/tmp/file.xlsx')
+    assert_equal %!{"sheet_name":"fish","sheet_index":2,"columns":[{"col":[]}],"base_template_path":"/tmp/file.xlsx"}!,json
   end
 
   def test_generator_call_with_quotes
@@ -67,12 +83,15 @@ class SampleTemplateGeneratorTest < Minitest::Test
     assert(File.exist?(@path))
   end
 
-  def test_with_base_template
-    base_template_path = File.join(File.dirname(__FILE__), 'files', 'test-base-template.xlsx')
+  def test_generator_with_base_template
     assert File.exist?(base_template_path), "cannot find base file at #{base_template_path}"
     json = { 'sheet_name' => 'fred', 'sheet_index' => 0, 'columns' => ['name' => %w(fred bob)], 'base_template_path' => base_template_path }.to_json
     refute(File.exist?(@path))
     Seek::SampleTemplates::Generator.new(@path, json).generate
     assert(File.exist?(@path))
+  end
+
+  def base_template_path
+    File.join(File.dirname(__FILE__), 'files', 'test-base-template.xlsx')
   end
 end
